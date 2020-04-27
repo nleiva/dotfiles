0a1e0eef37b0518c52820b4e6dca2eafb07946e8
Nicolas Leiva <nicolasleiva@gmail.com>
2020-04-27 17:11:08 -0400

    Update to Linux-only

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Bunch of symlinks
 ln -sfv "$DOTFILES_DIR/.aliases" ~
+ln -sfv "$DOTFILES_DIR/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/.bash_prompt" ~
 ln -sfv "$DOTFILES_DIR/.functions" ~
 ln -sfv "$DOTFILES_DIR/.inputrc" ~
-ln -sfv "$DOTFILES_DIR/.path" ~
